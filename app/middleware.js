96f06dfc4f889d9cca8c9b0a617d8a90e6a373ae
Zen <clonebmn2itt@gmail.com>
2019-10-16 12:58:50 +0000

    feat: custom morgan mode with node enviroment

--- app/middleware.js
+++ app/middleware.js
@@ -2,14 +2,19 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const express = require("express");
+const fs = require("fs");
 module.exports = function(app) {
-
-
+    const { NODE_ENV } = process.env;
+    const streamLog = fs.createWriteStream(path.resolve(__dirname, '../bin/request.log'), { flags: 'a' });
+    const logMiddleware = logger(NODE_ENV == "production" ? 'combined' : 'dev', {
+        // skip: (req, res) => res.statusCode < 400,
+        stream: NODE_ENV == "production" ? streamLog : process.stdout
+    })
     // view engine setup
     app.set('views', path.resolve(__dirname, '../views'));
     app.set('view engine', 'ejs');
 
-    app.use(logger('dev'));
+    app.use(logMiddleware);
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
     app.use(cookieParser());
